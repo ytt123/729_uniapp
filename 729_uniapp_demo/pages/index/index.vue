674b47816da388375990eb6302ddc4b0dc986b16
杨涛涛 <59249557+alentide@users.noreply.github.com>
2021-07-30 09:44:10 +0800

    add css

---
+++ 729_uniapp_demo/pages/index/index.vue
@@ -0,0 +1,72 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area ">
+			<text class="title">{{title}}</text>
+			<text class="gr-font13">{{title}}</text>
+
+			<text class="ftm60">123123</text>
+
+
+			<view class="gr-xyc itme">
+				<text class="gr-font40">123123</text>
+			</view>
+			<view class="gr-xyc itme">
+				<text class="gr-font13">123123</text>
+			</view>
+
+
+
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	.itme {
+		height: 200rpx;
+		width: 200rpx;
+		background-color: #4CD964;
+	}
+
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: $gr-color-main
+	}
+</style>
